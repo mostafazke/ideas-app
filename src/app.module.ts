3c20bf836e47129586a5812b4408acc4117b292e
Moustafa Zaki <moustafa.zaki@in3.cloud>
2019-11-28 19:59:52 +0200

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { IdeaModule } from './idea/idea.module';
 import { Connection } from 'typeorm';
 import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), IdeaModule, SharedModule],
+  imports: [TypeOrmModule.forRoot(), IdeaModule, SharedModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.entity.ts
@@ -0,0 +1,4 @@
+import { Entity } from 'typeorm';
+
+@Entity('user')
+export class UserEntity {}
