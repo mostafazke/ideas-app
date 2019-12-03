07f23e2e00e458200529caaed0e530ca9e789bdf
Moustafa Zaki <moustafa.zaki@in3.cloud>
2019-12-03 17:51:34 +0200

    add homepage

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ValidationPipe } from './shared/validation.pipe';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 dotenv.config();
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(port);
   Logger.log(`Server running on port ${port}`, 'bootstrap', true);
 }
